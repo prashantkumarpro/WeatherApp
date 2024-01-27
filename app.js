5897a88767a9c967f00bcccc9a6fce9d687ca4b7
Prashant Kumar <programmerkrprashant@gmail.com>
2024-01-27 21:51:17 +0530

    weathe app

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 
-async function weatheApp() {
+async function getWeather() {
     const apiKey = '970d587f393eafdffc284a76d86e4d4f'
     const city = document.querySelector('.city').value;
-
+  
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
     // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${city}&appid=${apiKey}`
 
@@ -18,6 +18,8 @@ async function weatheApp() {
         const response = await fetch(apiUrl);
         if (!response.ok) {
             console.log(`Error ${response.status} - ${response.statusText}`)
+            alert(`${response.statusText}`)
+            document.querySelector('.city').value = ''
             return;
         }
         const data = await response.json();
@@ -30,21 +32,33 @@ async function weatheApp() {
         document.querySelector('.city-name').textContent = `${name}`
         document.querySelector('.temperature').textContent = `${((temp) - (273.15)).toFixed(2)} ℃`
         document.querySelector('.wind').textContent = `: ${speed}`
-        document.querySelector('.weather-content-container').style.display = 'block'
+        let weatheContainer = document.querySelector('.weather-content-container');
+        let feelsLike = document.createElement('p');
+        feelsLike.textContent = `Feelslike ${((feels_like) - (273.15)).toFixed(2)} ℃`
+        feelsLike.classList.add('feels_like')
+        weatheContainer.append(feelsLike);
+        weatheContainer.style.display = 'block'
+        document.querySelector('.city').value = ''
+
+      
+      
 
     } catch (error) {
+        document.querySelector('.error-msg').textContent = `${error}`
         console.error('An error occurred:', error);
+        document.querySelector('.city').value = ''
     }
 }
 
 
-function displayCity() {
-    if (city.value === '') {
-        return alert('enter the city')
-    } else {
-        city.innerHTML = city.value;
-        city.value = ''
-        return;
-    }
+// function displayCity() {
+//     if (city.value === '') {
+//         return alert('enter the city')
+//     } else {
+//         city.innerHTML = city.value;
+//         city.value = ''
+//         return;
+//     }
 
-}
+// }
+
